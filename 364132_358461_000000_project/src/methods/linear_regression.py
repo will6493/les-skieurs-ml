5496da81c39d685e40f62faa034acb8f6550b346
FaatSquid <128168008+FaatSquid@users.noreply.github.com>
2024-04-14 18:28:54 +0200

    linear reg faite, j'espère. J'arrive pas à tester

--- 364132_358461_000000_project/src/methods/linear_regression.py
+++ 364132_358461_000000_project/src/methods/linear_regression.py
@@ -1,19 +1,20 @@
 import numpy as np
 import sys
 
+
 class LinearRegression(object):
     """
         Linear regressor object. 
         Note: This class will implement BOTH linear regression and ridge regression.
         Recall that linear regression is just ridge regression with lambda=0.
     """
-
     def __init__(self, lmda):
         """
             Initialize the task_kind (see dummy_methods.py)
             and call set_arguments function of this class.
         """
         self.lmda = lmda
+        self.weights = None
 
     def fit(self, training_data, training_labels):
         """
@@ -24,25 +25,22 @@ class LinearRegression(object):
             Returns:
                 pred_labels (np.array): target of shape (N,regression_target_size)
         """
-        pred_regression_targets = np.linalg.pinv(training_data) @ training_labels
+        inverseMatrix = np.linalg.inv(training_data.T @ training_data + self.lmda * np.eye(training_data.shape[1]))
+        self.weights = (inverseMatrix @ training_data.T) @ training_labels
+
+        pred_regression_targets = training_data @ self.weights
         return pred_regression_targets
 
+    def predict(self, test_data):
 
-def predict(self, test_data):
         """
             Runs prediction on the test data.
-            
+
             Arguments:
                 test_data (np.array): test data of shape (N,D)
             Returns:
                 test_labels (np.array): labels of shape (N,regression_target_size)
         """
-        ##
-        ###
-        #### YOUR CODE HERE!
-        ###
-        ##
+        pred_regression_targets = test_data @ self.weights
 
         return pred_regression_targets
-
-
--- 364132_358461_000000_project/test_ms1.py
+++ 364132_358461_000000_project/test_ms1.py
@@ -18,6 +18,7 @@ import numpy as np
 
 class HidePrints:
     """Disable normal printing for calling student code."""
+
     def __enter__(self):
         self._original_stdout = sys.stdout
         sys.stdout = open(os.devnull, 'w')
@@ -26,8 +27,10 @@ class HidePrints:
         sys.stdout.close()
         sys.stdout = self._original_stdout
 
+
 class NoHidePrints:
     """Don't disable normal printing for calling student code."""
+
     def __enter__(self):
         pass
 
@@ -40,7 +43,6 @@ class TestProject(unittest.TestCase):
     @staticmethod
     def title(msg):
         print(f"\n==============\n> {msg} ...")
-    
 
     def test_1_folder_structure(self):
         """Test the framework structure (folder and files)."""
@@ -51,7 +53,7 @@ class TestProject(unittest.TestCase):
         for file in ["main.py", "report.pdf"]:
             with self.subTest(f"Checking file {file}"):
                 self.assertTrue((project_path / file).exists(), f"No file {file} found at {project_path}")
-        
+
         # Source code
         src_path = project_path / "src"
         self.assertTrue(src_path.exists(), f"{src_path} not found")
@@ -66,7 +68,6 @@ class TestProject(unittest.TestCase):
             with self.subTest(f"Checking file methods/{file}"):
                 self.assertTrue((method_path / file).exists(), f"No file {file} found at {method_path}")
 
-    
     def _import_and_test(self, name, class_name, *args, **kwargs):
         """Test the import of the method and its functions."""
         # Code structure
@@ -74,7 +75,7 @@ class TestProject(unittest.TestCase):
         method = module.__getattribute__(class_name)(*args, **kwargs)
         for fn in ["fit", "predict"]:
             _ = method.__getattribute__(fn)
-        
+
         # Functions inputs and outputs
         N, D = 10, 3
         training_data = np.random.rand(N, D)
@@ -82,15 +83,18 @@ class TestProject(unittest.TestCase):
         test_data = np.random.rand(N, D)
         with no_print():
             pred_labels = method.fit(training_data, training_labels)
-        self.assertIsInstance(pred_labels, np.ndarray, f"{name}.{class_name}.fit() should output an array, not {type(pred_labels)}")
-        self.assertEqual(pred_labels.shape, training_labels.shape, f"{name}.{class_name}.fit() output has wrong shape ({pred_labels.shape} != {training_labels.shape})")
+        self.assertIsInstance(pred_labels, np.ndarray,
+                              f"{name}.{class_name}.fit() should output an array, not {type(pred_labels)}")
+        self.assertEqual(pred_labels.shape, training_labels.shape,
+                         f"{name}.{class_name}.fit() output has wrong shape ({pred_labels.shape} != {training_labels.shape})")
         with no_print():
             pred_labels = method.predict(test_data)
-        self.assertIsInstance(pred_labels, np.ndarray, f"{name}.{class_name}.predict() should output an array, not {type(pred_labels)}")
-        self.assertEqual(pred_labels.shape, training_labels.shape, f"{name}.{class_name}.predict() output has wrong shape ({pred_labels.shape} != {training_labels.shape})")
-        
-        return method
+        self.assertIsInstance(pred_labels, np.ndarray,
+                              f"{name}.{class_name}.predict() should output an array, not {type(pred_labels)}")
+        self.assertEqual(pred_labels.shape, training_labels.shape,
+                         f"{name}.{class_name}.predict() output has wrong shape ({pred_labels.shape} != {training_labels.shape})")
 
+        return method
 
     def test_2_dummy_methods(self):
         """Test the dummy methods."""
@@ -98,13 +102,12 @@ class TestProject(unittest.TestCase):
 
         _ = self._import_and_test("dummy_methods", "DummyClassifier",
                                   arg1=1)
-    
 
     def test_3a_knn(self):
         """Test K-Means."""
         self.title("Testing KNN")
 
-        knn_model = self._import_and_test("knn", "KNN",k=1)
+        knn_model = self._import_and_test("knn", "KNN", k=1)
 
         #  Test on easy dummy data
         training_data = np.array([[0., 0.], [1., 0.], [0., 1.], [1., 1.]])
@@ -116,30 +119,30 @@ class TestProject(unittest.TestCase):
             pred_labels_test = knn_model.predict(test_data)
         self.assertTrue(np.equal(pred_labels_train, training_labels).all(), f"KNN.fit() is not working on dummy data")
         self.assertTrue(np.equal(pred_labels_test, test_labels).all(), f"KNN.predict() is not working on dummy data")
-    
 
     def test_3b_logistic_regression(self):
         """Test Logistic Regression."""
         self.title("Testing Logistic Regression")
-
+        print("bite")
         logistic_regression = self._import_and_test("logistic_regression", "LogisticRegression",
                                                     lr=1e-3, max_iters=500)
 
-        # Test on easy dummy data
+        #  Test on easy dummy data
         N = 20
         training_data = np.concatenate([
-            np.linspace(-5, -0.25, N//2)[:,None],
-            np.linspace(0.25, 5, N//2)[:,None]
+            np.linspace(-5, -0.25, N // 2)[:, None],
+            np.linspace(0.25, 5, N // 2)[:, None]
         ], axis=0)
-        training_labels = (training_data[:,0] > 0.).astype(int)
+        training_labels = (training_data[:, 0] > 0.).astype(int)
         test_data = np.array([-10., -5., -1., 1., 5., 10.])[:, None]
-        test_labels = (test_data[:,0] > 0.).astype(int)
+        test_labels = (test_data[:, 0] > 0.).astype(int)
         with no_print():
             pred_labels_train = logistic_regression.fit(training_data, training_labels)
             pred_labels_test = logistic_regression.predict(test_data)
-        self.assertTrue((pred_labels_train == training_labels).all(), f"LogisticRegression.fit() is not working on dummy data")
-        self.assertTrue((pred_labels_test == test_labels).all(), f"LogisticRegression.predict() is not working on dummy data")
-        
+        self.assertTrue((pred_labels_train == training_labels).all(),
+                        f"LogisticRegression.fit() is not working on dummy data")
+        self.assertTrue((pred_labels_test == test_labels).all(),
+                        f"LogisticRegression.predict() is not working on dummy data")
 
     def test_3c_linear_regression(self):
         """Test Linear Regression."""
@@ -156,8 +159,10 @@ class TestProject(unittest.TestCase):
         with no_print():
             pred_labels_train = LR_model.fit(training_data, training_labels)
             pred_labels_test = LR_model.predict(test_data)
-        self.assertTrue(np.isclose(pred_labels_train, training_labels).all(), f"LinearRegression.fit() is not working on dummy data")
-        self.assertTrue(np.isclose(pred_labels_test, test_labels).all(), f"LinearRegression.predict() is not working on dummy data")
+        self.assertTrue(np.isclose(pred_labels_train, training_labels).all(),
+                        f"LinearRegression.fit() is not working on dummy data")
+        self.assertTrue(np.isclose(pred_labels_test, test_labels).all(),
+                        f"LinearRegression.predict() is not working on dummy data")
 
 
 def warn(msg):
@@ -170,7 +175,7 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--no-hide', action='store_true', help='Enable printing from the student code')
     args = parser.parse_args()
-    
+
     project_path = Path(".")
 
     dir_name = project_path.absolute().name
