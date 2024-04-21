5c33a86f59049b164f16b0ba0e2d36528cb414c7
FaatSquid <128168008+FaatSquid@users.noreply.github.com>
2024-04-21 18:35:04 +0200

    knn

--- 364132_358461_356001_project/src/methods/linear_regression.py
+++ 364132_358461_356001_project/src/methods/linear_regression.py
@@ -4,27 +4,30 @@ import sys
 
 class LinearRegression(object):
     """
-        Linear regressor object. 
-        Note: This class will implement BOTH linear regression and ridge regression.
-        Recall that linear regression is just ridge regression with lambda=0.
+    LinearRegression Object that performs linear or ridge regression depending on the lambda (lmda) parameter.
+    When lmda is 0, it performs ordinary least squares Linear Regression.
     """
     def __init__(self, lmda, task_kind = "regression"):
         """
-            Initialize the task_kind (see dummy_methods.py)
-            and call set_arguments function of this class.
+        Initializes the LinearRegression object with regularization strength lmda and task kind.
+
+        Parameters:
+            lmda (float): The regularization strength. Zero by default for linear regression.
+            task_kind (str): Type of task - "regression" or other kinds defined in extensions.
         """
         self.lmda = lmda
         self.task_kind = task_kind
         self.weights = None
 
+
     def fit(self, training_data, training_labels):
         """
-            Trains the model, returns predicted labels for training data.
-            Arguments:
-                training_data (np.array): training data of shape (N,D)
-                training_labels (np.array): regression target of shape (N,regression_target_size)
-            Returns:
-                pred_labels (np.array): target of shape (N,regression_target_size)
+        Trains the model, returns predicted labels for training data.
+        Arguments:
+            training_data (np.array): training data of shape (N,D)
+            training_labels (np.array): regression target of shape (N,regression_target_size)
+        Returns:
+            pred_labels (np.array): target of shape (N,regression_target_size)
         """
         inverseMatrix = np.linalg.inv(training_data.T @ training_data + self.lmda * np.eye(training_data.shape[1]))
         self.weights = (inverseMatrix @ training_data.T) @ training_labels
@@ -32,16 +35,16 @@ class LinearRegression(object):
         pred_regression_targets = training_data @ self.weights
         return pred_regression_targets
 
+
     def predict(self, test_data):
-
         """
-            Runs prediction on the test data.
+        Runs prediction on the test data.
 
-            Arguments:
-                test_data (np.array): test data of shape (N,D)
-            Returns:
-                test_labels (np.array): labels of shape (N,regression_target_size)
+        Arguments:
+            test_data (np.array): test data of shape (N,D)
+        Returns:
+            test_labels (np.array): labels of shape (N,regression_target_size)
         """
+
         pred_regression_targets = test_data @ self.weights
-
         return pred_regression_targets
